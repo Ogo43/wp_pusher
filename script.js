7d0e8aff3fe5ca78718db769de4c6b0cbcd47675
Ogo43 <emehaogochukwu@gmail.com>
2024-03-18 12:12:03 -0700

    My first commit

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+const menuBar = (e) => {
+  e.classList.toggle("change");
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.querySelector(".icon-burger");
+  const elementToToggle = document.getElementById("links");
+
+  button.addEventListener("click", () => {
+    elementToToggle.classList.toggle("hide");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.querySelector(".icon-burger");
+  const elementToToggle = document.getElementById("mobile-btn");
+
+  button.addEventListener("click", () => {
+    elementToToggle.classList.toggle("hide");
+  });
+});
+
 const questions = document.querySelectorAll(".questionaire");
 
 questions.forEach((questionaire) => {
